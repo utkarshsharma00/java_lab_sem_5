3906845d0320df133831c260e5994d8528f7aa75
Utkarsh Sharma <flamingice2801@gmail.com>
2019-10-09 20:42:15 +0530

    Update ThreadTest.java

--- code/ThreadTest.java
+++ code/ThreadTest.java
@@ -9,4 +9,22 @@ class A extends Thread {
     }
 }
 
-//yet to be written
+class B extends Thread {
+    public void run() {
+        for (int j = 0; j < 5; j++) {
+            System.out.println("From Thread B = " + j);
+            System.out.println("Exit from B");
+        }
+    }
+}
+
+class C extends Thread {
+    public void run() {
+        for (int k = 0; k < 5; k++) {
+            System.out.println("From Thread C = " + k);
+        }
+        System.out.println("Exit from C");
+    }
+}
+
+
