8b8aadec96f8a82bb9a177dab6628639fff189ec
Utkarsh Sharma <flamingice2801@gmail.com>
2019-11-30 18:45:37 +0530

    Create MultithreadingDemo.java

---
+++ code/MultithreadingDemo.java
@@ -0,0 +1,31 @@
+
+class MultithreadingDemo extends Thread 
+{ 
+	public void run() 
+	{ 
+		try
+		{ 
+			System.out.println ("Thread " + 
+				Thread.currentThread().getId() + 
+				" is running"); 
+
+		} 
+		catch (Exception e) 
+		{ 
+			System.out.println ("Exception is caught"); 
+		} 
+	} 
+} 
+
+public class Multithread 
+{ 
+	public static void main(String[] args) 
+	{ 
+		int n = 8; // Number of threads 
+		for (int i = 0; i < 8; i++) 
+		{ 
+			MultithreadingDemo object = new MultithreadingDemo(); 
+			object.start(); 
+		} 
+	} 
+}
