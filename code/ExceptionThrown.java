c2c93bedd1d160d45016e6e1177ca074e8c32187
Utkarsh Sharma <flamingice2801@gmail.com>
2019-12-03 14:12:09 +0530

    Update ExceptionThrown.java

--- code/ExceptionThrown.java
+++ code/ExceptionThrown.java
@@ -1,43 +1,36 @@
-import java.util.* 
+import java.util.*
 
-public class ExceptionThrown 
-{ 
-	static int divideByZero(int a, int b)
-	{ 
-		int i = a/b; 
-		return i; 
-	} 
-	 
-	static int computeDivision(int a, int b) 
-	{
-		int res = 0; 
-		
-		try
-		{ 
-		    res = divideByZero(a,b); 
-		} 
+public class ExceptionThrown {
+	static int divideByZero(int a, int b) {
+		int i = a / b;
+		return i;
+	}
 
-		catch(NumberFormatException ex) 
-		{ 
-		    System.out.println("NumberFormatException is occured"); 
+	static int computeDivision(int a, int b) {
+		int res = 0;
+
+		try {
+			res = divideByZero(a, b);
 		}
-		
-		return res; 
-	} 
-	
-	public static void main(String args[]){ 
-		
-		int a = 1; 
-		int b = 0; 
-		
-		try
-		{ 
-		     int i = computeDivision(a,b); 
-		} 
-		
-		catch(ArithmeticException ex) 
-		{ 
-		     System.out.println(ex.getMessage()); 
-		} 
-	} 
+
+		catch (NumberFormatException ex) {
+			System.out.println("NumberFormatException is occured");
+		}
+
+		return res;
+	}
+
+	public static void main(String args[]) {
+
+		int a = 1;
+		int b = 0;
+
+		try {
+			int i = computeDivision(a, b);
+		}
+
+		catch (ArithmeticException ex) {
+			System.out.println(ex.getMessage());
+		}
+	}
 }
