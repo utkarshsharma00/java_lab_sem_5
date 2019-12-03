5c4c61e96e09e669a75e7fbf693430d4a106d384
Utkarsh Sharma <flamingice2801@gmail.com>
2019-12-03 13:58:25 +0530

    Create File_notFound_Demo.java

---
+++ code/File_notFound_Demo.java
@@ -0,0 +1,19 @@
+import java.io.File; 
+import java.io.FileNotFoundException; 
+import java.io.FileReader; 
+
+class File_notFound_Demo { 
+
+	public static void main(String args[]) { 
+		try { 
+			// Following file does not exist 
+			File file = new File("E://file.txt"); 
+
+			FileReader fr = new FileReader(file); 
+		} 
+    
+    catch (FileNotFoundException e) { 
+		System.out.println("File does not exist"); 
+		} 
+	} 
+} 
