2350cf770cf692a691d132dcc00a639361662c53
Utkarsh Sharma <flamingice2801@gmail.com>
2019-12-03 14:13:21 +0530

    Update MultithreadingDemo.java

--- code/MultithreadingDemo.java
+++ code/MultithreadingDemo.java
@@ -1,32 +1,22 @@
 import java.util.*
 
-class MultithreadingDemo extends Thread 
-{ 
-	public void run() 
-	{ 
-		try
-		{ 
-			System.out.println ("Thread " + 
-				Thread.currentThread().getId() + 
-				" is running"); 
+class MultithreadingDemo extends Thread {
+	public void run() {
+		try {
+			System.out.println("Thread " + Thread.currentThread().getId() + " is running");
 
-		} 
-		catch (Exception e) 
-		{ 
-			System.out.println ("Exception is caught"); 
-		} 
-	} 
-} 
+		} catch (Exception e) {
+			System.out.println("Exception is caught");
+		}
+	}
+}
 
-public class Multithread 
-{ 
-	public static void main(String[] args) 
-	{ 
-		int n = 8; // Number of threads 
-		for (int i = 0; i < 8; i++) 
-		{ 
-			MultithreadingDemo object = new MultithreadingDemo(); 
-			object.start(); 
-		} 
-	} 
+public class Multithread {
+	public static void main(String[] args) {
+		int n = 8; // Number of threads
+		for (int i = 0; i < 8; i++) {
+			MultithreadingDemo object = new MultithreadingDemo();
+			object.start();
+		}
+	}
 }
